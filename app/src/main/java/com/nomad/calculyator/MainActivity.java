dfacc32b3dc21b86bed79ab73c817487d4744fb0
Kochevnikbbb <bbelekov@mail.ru>
2021-12-25 11:36:08 +0600

    всё переписал всё равно косячит

--- app/src/main/java/com/nomad/calculyator/MainActivity.java
+++ app/src/main/java/com/nomad/calculyator/MainActivity.java
@@ -4,26 +4,160 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView result;
-    private Integer first, second;
-    private Boolean isClickOperation = false;
-    private String operation;
-    //private Double res = 0.0;
+    private TextView tvResult;
+    private Float mValueOne, mValueTwo;
+    private Boolean crunchifyAddition, mSubtract, crunchifyMultiplication, crunchifyDivision;
+
+    private Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonPlus, buttonMinus, buttonDivide,
+            buttonMul, buttonTochka, buttonC, buttonEqual;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        result = findViewById(R.id.result);
+        tvResult = findViewById(R.id.result);
+        button0 = findViewById(R.id.zero);
+        button1 = findViewById(R.id.one);
+        button2 = findViewById(R.id.two);
+        button3 = findViewById(R.id.three);
+        button4 = findViewById(R.id.four);
+        button5 = findViewById(R.id.five);
+        button6 = findViewById(R.id.six);
+        button7 = findViewById(R.id.seven);
+        button8 = findViewById(R.id.eight);
+        button9 = findViewById(R.id.nine);
+        buttonTochka = findViewById(R.id.tochka);
+        buttonPlus = findViewById(R.id.plus);
+        buttonMinus = findViewById(R.id.minus);
+        buttonMul = findViewById(R.id.multiply);
+        buttonDivide = findViewById(R.id.divide);
+        buttonC = findViewById(R.id.clean);
+        buttonEqual = findViewById(R.id.equals);
+
+        button0.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "0");
+        });
+        button1.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "1");
+        });
+        button2.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "2");
+        });
+        button3.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "3");
+        });
+        button4.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "4");
+        });
+        button5.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "5");
+        });
+        button6.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "6");
+        });
+        button7.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "7");
+        });
+        button8.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "8");
+        });
+        button9.setOnClickListener(view ->
+
+        {
+            tvResult.setText(tvResult.getText() + "9");
+        });
+        buttonPlus.setOnClickListener(view ->
+
+        {
+            if (tvResult == null) {
+                tvResult.setText("");
+            } else {
+                mValueOne = Float.parseFloat(tvResult.getText() + "");
+                crunchifyAddition = true;
+                tvResult.setText(null);
+            }
+            buttonMinus.setOnClickListener(view1 -> {
+                mValueOne = Float.parseFloat(tvResult.getText() + "");
+                mSubtract = true;
+                tvResult.setText(null);
+            });
+            buttonMul.setOnClickListener(view1 -> {
+                mValueOne = Float.parseFloat(tvResult.getText() + "");
+                crunchifyMultiplication = true;
+                tvResult.setText(null);
+            });
+            buttonDivide.setOnClickListener(view1 -> {
+                mValueOne = Float.parseFloat(tvResult.getText() + "");
+                crunchifyMultiplication = true;
+                tvResult.setText(null);
+            });
+            buttonEqual.setOnClickListener(view1 -> {
+                mValueTwo = Float.parseFloat(tvResult.getText() + "");
+
+                if (crunchifyAddition == true) {
+                    tvResult.setText(mValueOne + mValueTwo + "");
+                    crunchifyAddition = false;
+                }
+
+                if (mSubtract == true) {
+                    tvResult.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (crunchifyMultiplication == true) {
+                    tvResult.setText(mValueOne * mValueTwo + "");
+                    crunchifyMultiplication = false;
+                }
+
+                if (crunchifyDivision == true) {
+                    tvResult.setText(mValueOne / mValueTwo + "");
+                    crunchifyDivision = false;
+                }
+            });
+            buttonC.setOnClickListener(view1 -> {
+                tvResult.setText("0");
+            });
+            buttonTochka.setOnClickListener(view1 -> {
+                tvResult.setText(tvResult.getText() + ".");
+            });
+        });
     }
 
 
+    public void onClickOperation(View view) {
+    }
+
     public void onClickNumber(View view) {
-/*        double[] numbers ={R.id.one,R.id.two,R.id.three,R.id.four,R.id.five,R.id.six,R.id.seven,R.id.eight,R.id.nine,R.id.zero,R.id.tochka,R.id.negative_number};
+    }
+
+
+    /*    public void onClickNumber(View view) {
+     *//*        double[] numbers ={R.id.one,R.id.two,R.id.three,R.id.four,R.id.five,R.id.six,R.id.seven,R.id.eight,R.id.nine,R.id.zero,R.id.tochka,R.id.negative_number};
         double x = 0;
 
         for (double i:numbers){
@@ -38,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
             }
-        }*/
+        }*//*
 
         switch (view.getId()) {
             case R.id.one:
@@ -207,6 +341,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-    }
+    }*/
 
 }
